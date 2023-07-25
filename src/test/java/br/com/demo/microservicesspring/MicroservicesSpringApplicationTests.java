ff6581cbdbf957929ddf80124a9ebe0b8e37702e
Renan Martins <fagundesm22@gmail.com>
2023-07-25 11:37:38 -0300

    Initial commit with base structure of the project.

---
+++ src/test/java/br/com/demo/microservicesspring/MicroservicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.microservicesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
